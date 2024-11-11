b8eeb441a044868556fdc9aa0308f3c8a71e1f35
Pidor <5608nik@mail.ru>
2024-11-11 17:45:59 +0700

    feat: довёл до ума header и добавил кнопку "вернуться на главную станицу" при переходе на не зарегистрированную ссылку

--- vue/src/views/NotFoundPage.vue
+++ vue/src/views/NotFoundPage.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class="text-center text-xl mt-5">
-      <h1>404 - Страница не найдена</h1>
-      <p>Извините, такой страницы не существует.</p>
-    </div>
-  </template>
+  <div class="text-center text-xl mt-5">
+    <h1>404 - Страница не найдена</h1>
+    <p>Извините, такой страницы не существует.</p>
+    <router-link 
+      to="/" 
+      class="mt-4 inline-block rounded bg-blue-500 text-white px-4 py-2 hover:bg-blue-600">
+      Вернуться на главную
+    </router-link>
+  </div>
+</template>
   
   <script>
   export default {
